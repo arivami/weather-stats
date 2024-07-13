bc6bb3cbf6c47b20cd6d64192853db9ce10a842a
Ari Zeltser <ari.zeltser@gmail.com>
2024-07-13 01:39:24 +0000

    continues seaorm implementation

---
+++ src/weather_data.rs
@@ -0,0 +1,26 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "weather_data")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i64,
+    pub zip: String,
+    pub city: String,
+    pub temperature: f64,
+    pub weather: String,
+    pub humidity: String,
+    pub wind_speed: f64,
+    pub measurement_time: DateTime,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter)]
+pub enum Relation {}
+
+impl RelationTrait for Relation {
+    fn def(&self) -> RelationDef {
+        panic!("No Relation")
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
