abccbea0c7f2d5cf6ffdfb3adc499d9749dcc877
Ari Zeltser <ari.zeltser@gmail.com>
2024-08-26 15:45:52 -0700

    added initialization queries for all tables

--- scripts/init.sql
+++ scripts/init.sql
@@ -40,3 +40,9 @@ create table UserZips (
     FOREIGN KEY (zip_code) REFERENCES TargetZips(zip_code),     
     FOREIGN KEY (user_id) REFERENCES Users(id) 
 );
+
+
+insert into Areas (name) VALUES ('San Jose'), ('Los Angeles');
+insert into TargetZips (zip_code, area_id) VALUES ('95124', 1), ('95014', 1), ('95123', 1), ('90046', 2), ('90291',2);
+insert into Users (id, name) VALUES (1, 'Bob'), (2, 'Joe'), (3, 'Dan');
+insert into UserZips (zip_code, user_id) VALUES ('95124', 1), ('95014', 2), ('90046', 3), ('95124', 2);
